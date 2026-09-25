d4ec8d883e46c4cd107d85fb7ee4c9746c6684cc
zayonrada <zayonrada@gmail.com>
2026-09-25 21:30:55 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 📌 เมื่อไหร่ที่อัปเดตโค้ดหน้าเว็บใหม่ ให้ขยับเลขตรงนี้ (เช่น v2 -> v3)
-const CACHE_NAME = 'pos-cache-v27';
+const CACHE_NAME = 'pos-cache-v28';
 
 const STATIC_ASSETS = [
   './',
